1f2357ecdc016b91d92f4e0f3404e4a57f9361e0
InfernoLum <42121738+InfernoLum@users.noreply.github.com>
2019-11-18 23:31:54 +0200

    fixed navmesh
    Added lerp to the camera position

--- Testing Github Things/Assets/Scripts/CameraFollow.cs
+++ Testing Github Things/Assets/Scripts/CameraFollow.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject follow;
+    public Vector3 offsets;
+    public float interpolation_amount = 0;
+
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lerpedPos = Vector3.Lerp(new Vector3(follow.transform.position.x + offsets.x,
+        follow.transform.position.y + offsets.y, follow.transform.position.z + offsets.z),gameObject.transform.position,interpolation_amount);
+        gameObject.transform.position = lerpedPos;
     }
 }
